d5acabe8474210276822e0efe2b2b3776f5eb221
WeiChien Hsu <weichien711@gmail.com>
2019-03-25 14:55:18 -0500

    :hammer: Created mock data file and export as a module

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mock_data = require('./mock_data');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.send(mock_data))
 
 app.get('/:name', (req, res) => res.send('Hi ' + req.params.name))
 
---
+++ mock_data.js
@@ -0,0 +1,51 @@
+module.exports = [{
+  "id": 1,
+  "title": "Feedfire",
+  "createdAt": "11/13/2018",
+  "finished": true
+}, {
+  "id": 2,
+  "title": "Skippad",
+  "createdAt": "2/19/2019",
+  "finished": false
+}, {
+  "id": 3,
+  "title": "Jaxworks",
+  "createdAt": "12/24/2018",
+  "finished": true
+}, {
+  "id": 4,
+  "title": "Flipstorm",
+  "createdAt": "10/11/2018",
+  "finished": true
+}, {
+  "id": 5,
+  "title": "Brainbox",
+  "createdAt": "1/1/2019",
+  "finished": true
+}, {
+  "id": 6,
+  "title": "Thoughtbeat",
+  "createdAt": "5/4/2018",
+  "finished": false
+}, {
+  "id": 7,
+  "title": "Oloo",
+  "createdAt": "8/9/2018",
+  "finished": true
+}, {
+  "id": 8,
+  "title": "Babblestorm",
+  "createdAt": "7/28/2018",
+  "finished": false
+}, {
+  "id": 9,
+  "title": "Aimbu",
+  "createdAt": "5/16/2018",
+  "finished": true
+}, {
+  "id": 10,
+  "title": "Omba",
+  "createdAt": "12/27/2018",
+  "finished": false
+}]
