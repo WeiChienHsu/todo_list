8da6726b26d4b62e38c5351706aa3b0d21692d0c
WeiChien Hsu <weichien711@gmail.com>
2019-03-29 20:46:43 -0500

    :hammer: Depreate bodyParser.json() - which is useless

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const port = 3000
 
 // Set up the body parser to convert requests
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
