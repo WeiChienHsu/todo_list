f85be94594e9664d353ad172b82d71118b2113e2
WeiChien Hsu <weichien711@gmail.com>
2019-04-10 15:24:00 -0500

    Set up passport Configuration - Strategy

---
+++ config/passport.js
@@ -0,0 +1,44 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+module.exports = (passport) => {
+  passport.use(
+    new LocalStrategy({ 
+      usernameField: 'email'
+    },
+    (email, password, done)) => {
+      /* Match user */
+      User.findOne({
+        email: email
+      }).then(user => {
+        /* User not found */
+        if(!user) {
+          return done(null, fall, {message, 'The email is not registered.'});
+        }
+
+        /* Match password */
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if(err) {
+            return done(null, false, { message: err })
+          }
+          if(isMatch) {
+            return done(null, user);
+          }
+          return done(null, false, { message: 'Password incorrect' });
+        });
+      });
+    }
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+}
