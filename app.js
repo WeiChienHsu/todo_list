24d0bb8d775eabb51b10b97ccd9fd701047b7932
WeiChien Hsu <weichien711@gmail.com>
2019-04-10 18:48:52 -0500

    Set up the Flash by using connect-flash
     - Allow using session
     - Set up three different messages type {success_msg, error_msg, error}
    in res.locals form req.flash
     - in Handlebars, we could use {#if error_msg} to retrieve the error
    message from session.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Task = require("./models/task")
 const handlebarsHelpers = require('./helpers/handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('passport')
 // const mock_data = require('./mock_data')
 
 const app = express()
@@ -15,6 +16,28 @@ const port = 3000
 // Set up the body parser to convert requests
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Connect flash
+app.use(flash())
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set up the View engine by Handlebars
 app.engine('handlebars', exphbs({
@@ -32,4 +55,4 @@ mongoose.connection.on('error', () => {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-require('./routes')(app)
+require('./routes')(app, passport)
