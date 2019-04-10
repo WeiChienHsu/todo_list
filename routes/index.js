0c75e0e2d9d2a295ee21bb908e7d32c13f03f8be
WeiChien Hsu <weichien711@gmail.com>
2019-04-09 22:43:17 -0500

    Set up the routing in index file with simple impl

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,46 @@
 const todoController = require('../controllers/todoController')
 
 module.exports = (app) => {
-  /* Index: display a list of all TASKs */
+  /* Index Page: Ask user to login  */
+  app.get('/', (req, res) => {
+    /* If not authenticated */
+    return res.send("This is Login Page")
+  })
+
+  /* Login Page: Ask user to login  */
+  app.get('/login', (req, res) => {
+    return res.send("This is Login Page")
+  })
+
+  app.post('/login', (req, res) => {
+    // Used passport to authenticate the user input
+    // Success -> redirect to /notes
+    // Failure -> Redirect to login
+  })
+
+  app.get('/signup', (req, res) => {
+    return res.send("This is Signup Page")
+  })
+
+
+  /* Create a new Account */
+  app.post('/signup', (req, res) => {
+    /* Check validation */
+
+    /* Check if the user is in the DB */
+
+    /* Create a new user */
+
+    /* Success -> Redirect to login */
+  })
+
+  app.get('/logout', (req, res) => {
+    // req.logout()
+    /* Redirect to login page */
+    return res.send("This is login Page")
+  })
+
+  /* Note Index: display a list of all TASKs */
   app.get('/notes', todoController.getAllTasks);
 
   /* Create: Create a new TASK when the form is submitted */
