c3882717b8c522152a1d955495d5a579e405a43c
WeiChien Hsu <weichien711@gmail.com>
2019-04-09 21:42:47 -0500

    Create todoController and init the methods
     - getAllTasks
     - createOneNewTask
     - getOneTaskByIdToEdit
     - updateOneTaskById
     - deleteOneTaskById
     - toggleOneTaskById

---
+++ controllers/todoController.js
@@ -0,0 +1,24 @@
+const Task = require("./models/task")
+
+let todoController = {
+  getAllTasks: (req, res) => {
+
+  },
+  createOneNewTask: (req, res) => {
+
+  },
+  getOneTaskByIdToEdit: (req, res) => {
+
+  },
+  updateOneTaskById: (req, res) => {
+
+  },
+  deleteOneTaskById: (req, res) => {
+
+  },
+  toggleOneTaskById: (req, res) => {
+    
+  }
+}
+
+module.exports = todoController
